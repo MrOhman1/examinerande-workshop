848f19f5514db5b8f97127c4d7a9d74fef0b4161
MrOhman1 <c25maxor@student.his.se>
2025-11-21 09:51:59 +0100

    Startmeny-scen, inkl UI. UI knappar. Scenbyte. Eget unity-event script

--- Assets/Scripts/Swap to play scene.cs
+++ Assets/Scripts/Swap to play scene.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Swaptoplayscene : MonoBehaviour
 {
+    public UnityEvent onstart;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        onstart.Invoke();
+        SceneManager.LoadScene(sceneName: "SampleScene");
     }
 
     // Update is called once per frame
