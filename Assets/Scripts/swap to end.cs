9d2664d6619996cecf0d498c65473e858ca7a52b
MrOhman1 <c25maxor@student.his.se>
2025-11-21 11:21:49 +0100

    Final build

---
+++ Assets/Scripts/swap to end.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class swaptoend : MonoBehaviour
+{
+    public UnityEvent onstart;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        onstart.Invoke();
+        SceneManager.LoadScene(sceneName: "end");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
